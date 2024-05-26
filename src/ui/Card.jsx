71949de863f167249ec073881450ddc93e717373
chanwl1988 <chanwl1988@gmail.com>
2024-05-26 02:33:48 +0000

    Committed to: 1. Add Card Section Header 2. Add Card header

--- src/ui/Card.jsx
+++ src/ui/Card.jsx
@@ -1,5 +1,16 @@
+/*
+  File name : Card.jsx 
+  Student’s Name : Wai Lung Chan 
+  StudentID : 301325509 
+  Date : 26-May-2024
+*/
+
 import classes from './Card.module.css';
 
+/*
+  Section Header
+  Export function for Card UI
+*/
 function Card(props) {
   return <div className={classes.card}>{props.children}</div>;
 }
